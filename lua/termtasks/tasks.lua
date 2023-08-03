6f38380c7edab00c723fe4c1c14d36235edf892e
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-08-03 09:47:38 +0530

    added some regex detection

--- lua/termtasks/tasks.lua
+++ lua/termtasks/tasks.lua
@@ -7,17 +7,37 @@ function M.getAvailableTasks()
 		print("Could not find any Taskfiles in the git root or pwd")
 		return nil
 	end
+	local taskList = {}
+	for _, fname in ipairs(files) do
+		local tasks = parseTaskFile(fname)
+		if tasks ~= nil then
+			for _, task in ipairs(tasks) do
+				table.insert(taskList, task)
+			end
+		end
+	end
+	return taskList
+end
+
+function parseTaskFile(fname)
 	local tasks = {}
-	print(vim.inspect(files))
-	print("Some tasks found!!")
+	if string.match(fname, "Taskfile.yml") then
+		local contents = io.popen("task --list-all 2> /dev/null", "r")
+		for line in contents:lines() do
+			for task in string.gmatch(line, '%*%s(.*):') do
+				table.insert(tasks, "Taskfile.yml:" .. task)
+			end
+		end
+	end
 	return tasks
 end
 
 function getWorkDir()
-	local dir = io.popen('git rev-parse --show-toplevel'):read()
-	if dir == nil then
-		dir = io.popen('pwd'):read()
+	local dir = io.popen('git rev-parse --show-toplevel 2> /dev/null'):read()
+	if dir ~= nil then
+		return dir
 	end
+	dir = io.popen('pwd'):read()
 	return dir
 end
 
